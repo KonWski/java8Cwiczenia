d5d5850d9c5fa52f029f2d562891378171e2f679
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2018-03-20 19:57:29 +0100

    Rozdział 3 zad 1(2)

---
+++ Rozwiazania/src/Rozdzial3/Zadanie1/Employee.java
@@ -0,0 +1,56 @@
+package Rozdzial3.Zadanie1;
+
+/**
+ * Zaimplementuj interfejs Measurable w klasie Employee
+ */
+
+public class Employee implements Measurable{
+
+    String firstName;
+    String lastName;
+    Integer age;
+    Double salary;
+
+    public Employee(String firstName, String lastName, Integer age, Double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public double getMeasure() {
+        return salary * 12;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+}
---
+++ Rozwiazania/src/Rozdzial3/Zadanie1/Measurable.java
@@ -0,0 +1,11 @@
+package Rozdzial3.Zadanie1;
+
+/**
+ * Utwórz interfejs z metodą double getMeasure(), która dostarcza jakąś metrykę obiektu.
+ */
+
+public interface Measurable {
+
+    double getMeasure();
+
+}
